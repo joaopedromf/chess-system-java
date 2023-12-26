4383c69106715992fdc6f501c1703403078604a4
João Pedro Martins Forte <jpforte2004@gmail.com>
2023-12-26 20:34:49 -0300

    Começando a implementar Tabuleiro e Peca

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,11 +1,11 @@
 package application;
 
 import boardgame.Posicao;
+import boardgame.Tabuleiro;
 
 public class Programa {
     public static void main(String[] args) {
         
-        Posicao pos = new Posicao(3, 5);
-        System.out.println(pos);
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
     }
 }
